012043130aeea53028daab0cb982dbde54f36630
admin <13539798574@163.com>
2018-04-18 15:28:50 +0800

    首次提交

---
+++ BiYingSpider.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+_Author_ = 'Alvis'
+_Date_ = '2018-04-18 15:14'
+
+import requests
+import re
+import time
+local = time.strftime("%Y.%m.%d")
+url = 'http://cn.bing.com/'
+con = requests.get(url)
+content = con.text
+reg = r"(http://s.cn.bing.net/az/hprichbg/rb/.*?.jpg)"
+a = re.findall(reg, content, re.S)[0]
+print(a)
+read = requests.get(a)
+f = open('%s.jpg' % local, 'wb')
+f.write(read.content)
+f.close()

3341bcc2da216318ed05573f1dee3f73ca76917a
Alvis <alvisblog@alvisblog.xyz>
2018-04-18 21:21:43 +0800

    无修改

--- BiYingSpider.py
+++ BiYingSpider.py
@@ -9,7 +9,6 @@ local = time.strftime("%Y.%m.%d")
 url = 'http://cn.bing.com/'
 html= requests.get(url).text
 link= re.findall(u"""g_img={url: "(.*?)",""", html)
-print(link)
 piclink='%s%s'%(url,link[0])
 print(piclink)
 picture = requests.get(piclink)

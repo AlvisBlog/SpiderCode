eebb3097b12e20b8dec16aa79012e295976bd41c
Alvis <alvisblog@alvisblog.xyz>
2018-05-03 15:43:34 +0800

    新增获取作者，最后更新时间，最近一章，小说状态

--- Spider_Novel_XXBQG.py
+++ Spider_Novel_XXBQG.py
@@ -18,8 +18,14 @@ class Spider_bqg_novel:
         self.chapter_url=[]
         #章节内容
         self.chapter_content=[]
-        #三者合并
-        self.chapter_info=[]
+        #小说作者
+        self.author=[]
+        #小说状态
+        self.status=[]
+        #小说最后更新时间
+        self.last_update_time=[]
+        #小说最近更新的章节名称
+        self.last_chapter_name=[]
         self.chapter_num=1
 
     #获取章节名称及链接、内容,需要传入小说页面链接
@@ -29,6 +35,14 @@ class Spider_bqg_novel:
         urllib3.disable_warnings()
         # 源网页代码
         html = response.text.encode('ISO-8859-1').decode('utf8')
+        #小说状态
+        self.status=re.findall('<meta property="og:novel:status" content="(.*?)"/>',html,re.S)
+        #获取作者
+        self.author=re.findall('<meta property="og:novel:author" content="(.*?)"/>',html,re.S)
+        #获取最后更新时间
+        self.last_update_time=re.findall('<meta property="og:novel:update_time" content="(.*?)"/>',html,re.S)
+        #获取最近更新的章节名称
+        self.last_chapter_name=re.findall('<meta property="og:novel:latest_chapter_name" content="(.*?)"/>',html,re.S)
         # 网页中包含章节名称及链接的数据
         content = re.findall('<dl>(.*?)</dl>', html, re.S)
         # 获取章节部分链接，缺少"https://www.xxbiquge.com"
@@ -55,7 +69,7 @@ class Spider_bqg_novel:
                 print("第%s章节内容为空,无法获取"%self.chapter_num)
             self.chapter_num=self.chapter_num+1
             #每隔5秒获取一次章节内容
-            sleep(5)
+            # sleep(5)
         return self.chapter_url, self.chapter_name, self.chapter_content
 
     # 保存数据,需要传入小说名称,excel文件名称
@@ -79,6 +93,14 @@ class Spider_bqg_novel:
         ws.cell(row=1, column=1, value='章节名称')
         ws.cell(row=1, column=2, value='章节地址')
         ws.cell(row=1, column=3, value='章节内容')
+        ws.cell(row=1, column=4, value='小说作者')
+        ws.cell(row=2, column=4, value=self.author[0])
+        ws.cell(row=1, column=5, value='小说状态')
+        ws.cell(row=2, column=5, value=self.status[0])
+        ws.cell(row=1, column=6, value='最后更新时间')
+        ws.cell(row=2, column=6, value=self.last_update_time[0])
+        ws.cell(row=1, column=7, value='最新一章')
+        ws.cell(row=2, column=7, value=self.last_chapter_name[0])
         for i in range(len(self.chapter_name)):
             ws.cell(row=i + 2, column=1, value=self.chapter_name[i])
         for i in range(len(self.chapter_url)):
@@ -91,7 +113,7 @@ if __name__=="__main__":
     print("正在实例化对象")
     Spider=Spider_bqg_novel()
     print("正在获取章节信息")
-    Spider.GetChapterData("https://www.xxbiquge.com/80_80689/")
+    Spider.GetChapterData("https://www.xxbiquge.com/79_79590/")
     print(len(Spider.chapter_content),len(Spider.chapter_name),len(Spider.chapter_url))
     print("正在存储数据")
-    Spider.SaveNovelDataToExcel("透视小野医","笔趣阁小说")
+    Spider.SaveNovelDataToExcel("剑镇鸿蒙","笔趣阁小说")

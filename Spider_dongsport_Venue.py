15bf89f1340984ed5a3e451f8ba61dfdac81283c
initially <1257042352@qq.com>
2018-05-04 15:43:13 +0800

    爬取动网场馆信息

---
+++ Spider_dongsport_Venue.py
@@ -0,0 +1,64 @@
+#coding=utf8
+_Author_ = 'Alvis'
+_Date_ = '14:40 2018-05-04'
+
+import requests
+import re
+from requests.packages import urllib3
+import openpyxl
+
+#球馆名称
+venue_name=[]
+#球馆地址
+venue_address=[]
+#球馆标签
+venue_tag=[]
+#球馆电话
+venue_mobile=[]
+
+def GetVenueInfo():
+    for page in range(1,410):
+        print("当前访问第%s"%page)
+        url='http://www.dongsport.com/venue/list-1004401-0-0-0-0-0-0-0-%s.html'%page
+        try:
+            response=requests.get(url)
+            urllib3.disable_warnings()
+        except Exception as error:
+            with open("log.text","a+") as f:
+                f.write("无法访问第%s页"%page+"错误为:%s"%error+"\n")
+            continue
+        html = response.text
+        venue_content=re.findall('<div class="left v_l_text">(.*?)</div>',html,re.S)
+        for content in venue_content:
+            venue_name.append(re.findall('target="_blank">(.*?) ',content,re.S)[0].strip())
+            venue_address.append(re.findall('<li>(.*?) ',content,re.S)[0].strip())
+            venue_tag.append(re.findall('<li>(.*?)</li>',content,re.S)[2].strip())
+            venue_mobile.append(re.findall('<b class="fontstyle4">(.*?) ',content,re.S)[0].strip())
+
+def SaveVenueInfo():
+    try:
+        wb=openpyxl.load_workbook(filename="场馆信息.xlsx")
+    except Exception as e:
+        wb=openpyxl.Workbook()
+    if 'Sheet' in wb.sheetnames:
+        del wb.sheetnames['Sheet']
+    ws=wb.create_sheet()
+    ws.title='动网'
+    ws.cell(row=1, column=1, value='场馆名称')
+    ws.cell(row=1, column=2, value='场馆地址')
+    ws.cell(row=1, column=3, value='场馆电话')
+    ws.cell(row=1, column=4, value='场馆标签')
+    for i in range(len(venue_mobile)):
+        ws.cell(row=i + 2, column=1, value=venue_name[i])
+        ws.cell(row=i + 2, column=2, value=venue_address[i])
+        ws.cell(row=i + 2, column=3, value=venue_mobile[i])
+        ws.cell(row=i + 2, column=4, value=venue_tag[i])
+        print("已写入%s条数据"%i)
+    wb.save("场馆信息.xlsx")
+
+def run():
+    GetVenueInfo()
+    SaveVenueInfo()
+
+if __name__=="__main__":
+    run()
